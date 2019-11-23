719286de10a4d600602ffe9a935526a278161852
Ihor Tretiak <igor.tretyak358@gmail.com>
2019-11-23 21:51:05 +0200

    add rest api for posts and httpauth

--- flaskr/auth/auth.py
+++ flaskr/auth/auth.py
@@ -1,4 +1,5 @@
 import functools
+import json
 from flask import jsonify
 from flask import Blueprint
 from flask import flash
@@ -9,103 +10,72 @@ from flask import request
 from flask import session
 from flask import url_for
 from werkzeug.security import check_password_hash
-# TODO: Use https://flask-httpauth.readthedocs.io/en/latest/
+from flask_httpauth import HTTPBasicAuth
+from flask import Response
 
 from flaskr.db import get_db
 from flaskr.auth.queries import (
     create_user, get_user_by_id, get_user_by_username
 )
 
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    user = get_user_by_username(get_db(), username)
+    if user:
+        return check_password_hash(user["password"], password)
+    return False
+
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-def login_required(view):
-    """View decorator that redirects anonymous users to the login page."""
-
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for("auth.login"))
-
-        return view(**kwargs)
-
-    return wrapped_view
-
-
-@bp.before_app_request
-def load_logged_in_user():
-    """If a user id is stored in the session, load the user object from
-    the database into ``g.user``."""
-    user_id = session.get("user_id")
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_user_by_id(get_db(), user_id)
-
-
-@bp.route("/register", methods=("GET", "POST"))
+@bp.route("/register", methods=["POST"])
 def register():
     """Register a new user.
 
     Validates that the username is not already taken. Hashes the
     password for security.
     """
-    if request.method == "POST":
-        db = get_db()
-        error = None
+    db = get_db()
+    error = None
+        
+    data = request.get_json()
+    username = data.get('username', '')
+    password = data.get('password', '')
 
-        username = request.form['username']
-        password = request.form['password']
+    if not username:
+        error = "Username is required."
+    elif not password:
+        error = "Password is required."
+    elif get_user_by_username(db, username) is not None:
+        error = "User {0} is already registered.".format(username)
 
-        if not username:
-            error = "Username is required."
-        elif not password:
-            error = "Password is required."
-        elif get_user_by_username(db, username) is not None:
-            error = "User {0} is already registered.".format(username)
-
-        if error is None:
-            # the name is available, store it in the database and go to
-            # the login page
-            create_user(db, username, password)
-            return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template("auth/register.html")
-
-
-@bp.route("/login", methods=("GET", "POST"))
-def login():
-    """Log in a registered user by adding the user id to the session."""
-    if request.method == "POST":
-        db = get_db()
-        error = None
-
-        username = request.form['username']
-        password = request.form['password']
-
-        user = get_user_by_username(db, username)
-
-        if user is None:
-            error = "Incorrect username."
-        elif not check_password_hash(user["password"], password):
-            error = "Incorrect password."
-
-        if error is None:
-            # store the user id in a new session and return to the index
-            session.clear()
-            session["user_id"] = user["id"]
-            return redirect(url_for("index"))
-
-        flash(error)
-
-    return render_template("auth/login.html")
+    if error:
+        error = json.dumps({"error": error})
+        return Response(
+            error,
+            status=400,
+            mimetype="application/json"
+        )
+    
+    create_user(db, username, password)
+    user = get_user_by_username(db, username)
+    data = {"user_id": user["id"], "username": user["username"]}
+    data = json.dumps(data)
+    return Response(
+        data,
+        status=200,
+        mimetype="application/json"
+    )
 
 
 @bp.route("/logout")
+@auth.login_required
 def logout():
-    """Clear the current session, including the stored user id."""
-    session.clear()
-    return redirect(url_for("index"))
+    data = json.dumps({'response': 'Logout successfully'})
+    return Response(
+        data,
+        status=401,
+        mimetype="application/json"
+    )
--- flaskr/blog/blog.py
+++ flaskr/blog/blog.py
@@ -5,25 +5,18 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
-from flask import jsonify
+from flask import Response
 from werkzeug.exceptions import abort
+import json
 
-from flaskr.auth.auth import login_required
+from flaskr.auth.auth import auth
 from flaskr.db import get_db
+from flaskr.auth.queries import get_user_by_username
 from flaskr.blog.queries import (
     create_post, delete_post, get_post, update_post, post_list
 )
 
 bp = Blueprint("blog", __name__)
-
-
-@bp.route("/")
-def index():
-    """Show all the posts, most recent first."""
-    db = get_db()
-    posts = post_list(db)
-    return jsonify({'posts': posts})
-
 
 def check_post(id, check_author=True):
     """Get a post and its author by id.
@@ -39,72 +32,139 @@ def check_post(id, check_author=True):
     """
 
     post = get_post(get_db(), id)
+
     if post is None:
         abort(404, "Post id {0} doesn't exist.".format(id))
 
-    if check_author and post["author_id"] != g.user["id"]:
+    username = auth.username()
+    user = get_user_by_username(get_db(), username)
+
+    if not user:
+        abort(403)
+
+    if check_author and post["author_id"] != user["id"]:
         abort(403)
 
     return post
 
 
-@bp.route("/create", methods=("POST",))
-@login_required
-def create():
-    """Create a new post for the current user."""
-
+@bp.route("/posts", methods=["GET", "POST"])
+@auth.login_required
+def post_list():
+    db = get_db()
     error = None
 
-    title = request.json.get('title', '')
-    body = request.json.get('body', '')
+    # get all posts
+    if request.method == "GET":
+        posts = post_list(db)
 
-    if not title:
-        error = "Title is required"
+        if not posts:
+            error = json.dumps({"error": "No posts available."})
+        if error:
+            return Response(
+                error,
+                status=404,
+                mimetype="application/json"
+            )
 
-    if error is not None:
-        flash(error)
-    else:
+        data = json.dumps([dict(post) for post in posts])
+        return Response(
+            data,
+            status=200,
+            mimetype="application/json"
+        )
+    
+    # create new post
+    elif request.method == "POST":
+        data = request.get_json()
+        title = data.get('title', '')
+        body = data.get('body', '')
+
+        if not title:
+            error = json.dumps({"error": "Title is required"})
+        if error:
+            return Response(
+                error,
+                status=400,
+                mimetype="application/json"
+            )
+
         db = get_db()
-        create_post(db, title, body, g.user["id"])
-        # return redirect(url_for("blog.index"))
-        return jsonify({'post': {'title': title, 'body': body}}), 201
+        username = auth.username()
+        user = get_user_by_username(username)
+        create_post(db, title, body, user["id"])
+        data = json.dumps({'title': title,'body': body,
+                           'user_id': user["id"]})
+        return Response(
+            data,
+            status=201,
+            mimetype="application/json"
+        )
+
+    error = json.dumps({"error": 'Unknown method'})
+    return Response(
+        error,
+        stauts=405,
+        mimetype="application/json"
+    )
 
 
-
-@bp.route("/<int:id>/update", methods=("POST",))
-@login_required
-def update(id):
-    """Update a post if the current user is the author."""
+@bp.route('/posts/<int:id>', methods=["GET", "PUT", "DELETE"])
+@auth.login_required
+def post():
     post = check_post(id)
-
     error = None
 
-    title = request.json.get('title', '')
-    body = request.json.get('body', '')
+    # post with id does not exist
+    if not post:
+        error = json.dumps({"error": "Post id {0} not available.".format(id)})
+    if error:
+        return Response(
+            error,
+            status=404,
+            mimetype="application/json"
+        )
 
-    if not title:
-        error = "Title is required"
+    # get post by id
+    if request.method == "GET":
+        data = json.dumps(dict(post))
+        return Response(
+            data,
+            status=200,
+            mimetype="application/json"
+        )
 
-    if error is not None:
-        flash(error)
-    else:
+    # update post by id
+    elif request.method == "PUT":
+        data = request.get_json()
+        title = data.get('title', '')
+        body = data.get('body', '')
+
+        if not title:
+            error = json.dumps({"error": "Title is required"})
+        if error:
+            return Response(
+                error,
+                status=400,
+                mimetype="application/json"
+            )
+
         db = get_db()
         update_post(db, title, body, id)
-        # return redirect(url_for("blog.index"))
-        return jsonify({'message': 'post with id %s updated' % id})
-
-
-
-@bp.route("/<int:id>/delete", methods=("POST",))
-@login_required
-def delete(id):
-    """Delete a post.
-
-    Ensures that the post exists and that the logged in user is the
-    author of the post.
-    """
-    check_post(id)
-    db = get_db()
-    delete_post(db, id)
-    # return redirect(url_for("blog.index"))
-    jsonify({'message': 'post with id %s deleted' % id})
+        message = json.dumps({'message': 'Post id {0} update successfully'.format(id)})
+        return Response(
+            message,
+            status=200,
+            mimetype="application/json"
+        )
+    
+    # delete post by id
+    elif request.method == "DELETE":
+        db = get_db()
+        delete_post(db, id)
+        message = json.dumps({'message': 'Post id {0} delete successfully'.format(id)})
+        return Response(
+            message,
+            status=200,
+            mimetype="application/json"
+        )
